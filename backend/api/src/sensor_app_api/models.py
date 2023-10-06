59a4c06a264768a640f18c2fbe68afb6f59360b3
aambrose-unh <aambrose.unh@gmail.com>
2023-10-06 05:47:03 -0400

    update db models

--- backend/api/src/sensor_app_api/models.py
+++ backend/api/src/sensor_app_api/models.py
@@ -12,24 +12,40 @@ class Sensor(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(64))
     description: Mapped[str] = mapped_column(String(128))
+    measurement_type_id: Mapped[int] = mapped_column(ForeignKey("measurement_type.id"))
 
+    measurement_types = relationship("MeasurementType", back_populates="sensors")
     outputs = relationship("SensorOutput", back_populates="sensor")
 
     def __repr__(self) -> str:
-        return f"Sensor(id={self.id!r}, name={self.name!r}, description={self.description!r})"
+        return f"Sensor(id={self.id!r}, name={self.name!r}, \
+            description={self.description!r})"
 
 
 class SensorOutput(Base):
     __tablename__ = "sensor_output"
     id: Mapped[int] = mapped_column(primary_key=True)
-    temperature: Mapped[float] = mapped_column(Double)
-    humidity: Mapped[float] = mapped_column(Double)
-    dateMeasured: Mapped[datetime.date]
-    hourMeasured: Mapped[int]
+    value: Mapped[float] = mapped_column(Double)
+    date_measured: Mapped[datetime.date]
+    hour_measured: Mapped[int]
     sensor_id: Mapped[int] = mapped_column(ForeignKey("sensors.id"))
 
     sensor = relationship("Sensor", back_populates="outputs")
 
     def __repr__(self) -> str:
-        return f"SensorOutput(id={self.id!r}, sensorid={self.sensorid!r}, temperature={self.temperature!r}, \
-                humidity={self.humidity!r}, dateMeasured={self.dateMeasured!r}, hourMeasured={self.hourMeasured!r})"
+        return f"SensorOutput(id={self.id!r}, sensorid={self.sensorid!r}, \
+                value={self.value!r}, \
+                date_measured={self.date_measured!r}, hour_measured={self.hour_measured!r})"
+
+
+class MeasurementType(Base):
+    __tablename__ = "measurement_type"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(64))
+    description: Mapped[str] = mapped_column(String(128))
+
+    sensors = relationship("Sensor", back_populates="measurement_types")
+
+    def __repr__(self) -> str:
+        return f"MeasurementType(id={self.id!r}, name={self.name!r},\
+              description={self.description!r})"
