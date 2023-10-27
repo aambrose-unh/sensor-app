3d37c6324d5e69c66e5b9f3b4bf7a44c27318905
aambrose-unh <aambrose.unh@gmail.com>
2023-10-27 12:41:08 -0400

    update connection string for db

--- backend/api/src/sensor_app_api/database.py
+++ backend/api/src/sensor_app_api/database.py
@@ -7,11 +7,12 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWD = os.getenv("DB_PASSWD")
 DB_NAME = os.getenv("DB_NAME")
 DB_PORT = os.getenv("DB_PORT")
+DB_HOST = os.getenv("DB_HOST")
 
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 SQLALCHEMY_DATABASE_URL = (
-    f"mysql+pymysql://{DB_USER}:{DB_PASSWD}@localhost:{DB_PORT}/{DB_NAME}"
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
